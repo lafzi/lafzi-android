08d971bc99f4f848b40ed08b7d0d80d8cd7e24e8
abrari <muhammad.abrari@gmail.com>
2017-04-28 07:43:05 +0700

    toolbar for activities

--- app/src/main/java/com/lafzi/lafzi/activities/AboutActivity.java
+++ app/src/main/java/com/lafzi/lafzi/activities/AboutActivity.java
@@ -3,6 +3,8 @@ package com.lafzi.lafzi.activities;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.lafzi.lafzi.R;
 
@@ -16,5 +18,23 @@ public class AboutActivity extends AppCompatActivity{
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        Toolbar mainToolbar = (Toolbar) findViewById(R.id.about_toolbar);
+        setSupportActionBar(mainToolbar);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
--- app/src/main/java/com/lafzi/lafzi/activities/SettingActivity.java
+++ app/src/main/java/com/lafzi/lafzi/activities/SettingActivity.java
@@ -1,23 +1,45 @@
 package com.lafzi.lafzi.activities;
 
 import android.os.Bundle;
-import android.preference.PreferenceActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
+import com.lafzi.lafzi.R;
 import com.lafzi.lafzi.fragments.SettingFragment;
 
 /**
  * Created by alfat on 23/04/17.
  */
 
-public class SettingActivity extends PreferenceActivity {
+public class SettingActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+
+        Toolbar mainToolbar = (Toolbar) findViewById(R.id.setting_toolbar);
+        setSupportActionBar(mainToolbar);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
 
         getFragmentManager()
                 .beginTransaction()
-                .replace(android.R.id.content, new SettingFragment())
+                .replace(R.id.setting_content_frame, new SettingFragment())
                 .commit();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
